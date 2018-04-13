9ad1321ed0ca87b1194ec105b562460c8a095ee7
nguyendangkhanh <khanh.nv001@gmail.com>
2018-04-13 15:29:15 +0700

    add listkey with map

--- helloreact/src/ListsKey.js
+++ helloreact/src/ListsKey.js
@@ -5,15 +5,38 @@ export function ShowNumber()
     return 2;
 }
 
-export const posts = [
+const contentPosts = [
     {id: 1, title: 'This is row A', content: 'Hello Row A'},
     {id: 2, title: 'This is row B', content: 'Hello Row B'},
 ];
 
+function ListItem(props)
+{
+    return <li>{props.value}</li>;
+}
+
 export function ShowContentPosts(props)
 {
-    const posts = props.numbers;
-    const listPosts = posts.map((post) => <li key={post.toString()}>{post}</li>);
+    const posts = props.content;
+   // const listPosts = posts.map((post) => <li key={post.id}>{post.title}</li>);
 
-    return (<ul>{listPosts}</ul>);
+  //  return (<ul>{listPosts}</ul>);
+    return (
+        <ul>{
+            posts.map((post) => <ListItem id={post.id} value={post.title} />)
+        }</ul>
+    );
+}
+
+export class BlogContent extends Component{
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return(
+            <div><ShowContentPosts content={contentPosts} /></div>
+        );
+    }
+    
 }
--- helloreact/src/index.js
+++ helloreact/src/index.js
@@ -3,12 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import Page from './Page';
-import {ShowContentPosts} from './ListsKey';
+import {ShowContentPosts, BlogContent} from './ListsKey';
 import registerServiceWorker from './registerServiceWorker';
 
-const numbers = [1,2,3,4,5];
 ReactDOM.render(<App />, document.getElementById('root'));
 ReactDOM.render(<Page />, document.getElementById('banner'));
-ReactDOM.render(<ShowContentPosts numbers={numbers} />, document.getElementById('list'));
+ReactDOM.render(<BlogContent />, document.getElementById('list'));
 registerServiceWorker();
  
